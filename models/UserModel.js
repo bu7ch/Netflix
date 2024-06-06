9fc82c4ecca06dc9ca1fbfd4ae1d3c840f695841
Rodolphe <dutchr@live.fr>
2024-06-06 11:40:42 +0200

    :sparkles: Adding userModel

---
+++ models/UserModel.js
@@ -0,0 +1,20 @@
+const { mongoose, model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
